512b5c2ddff980b80d4c69ef9ae7346ca0ebeb9b
hoonhoonhoon <hoonhoon@woowahan.com>
2018-05-29 12:55:06 +0900

    update RxSwiftCrashTest

--- SwiftOptimiztion/SwiftOptimiztionTests/RxSwiftCrashTest.swift
+++ SwiftOptimiztion/SwiftOptimiztionTests/RxSwiftCrashTest.swift
@@ -10,15 +10,25 @@ import XCTest
 import RxSwift
 @testable import SwiftOptimiztion
 
+final class TestObserver<Element>: ObserverType {
+
+	func on(_ event: Event<Element>) {
+		print(event)
+	}
+
+}
+
 class RxSwiftCrashTest: XCTestCase {
 
-	var subject: PublishSubject<Bool>!
+	var publishSubject: PublishSubject<Bool>!
+	var testObserver: TestObserver<Bool>!
 	var bag: DisposeBag!
 
 	override func setUp() {
 		super.setUp()
 		bag = DisposeBag()
-		subject = PublishSubject<Bool>()
+		publishSubject = PublishSubject<Bool>()
+		testObserver = TestObserver()
 	}
 
 	override func tearDown() {
@@ -28,7 +38,8 @@ class RxSwiftCrashTest: XCTestCase {
 
 	// onNext가 가능한 Rx trait를 다른 옵저버블에 바인딩 하지 않고 직접 해당 trait의 onNext함수를 인수로 전달할 경우 해당 함수를 소유하는 trait이 메모리에서 해지되거나 메모리 좌표에 문제가 발생하는 듯
 	func testRxSwiftCrash() {
-		Observable.just(true).subscribe(onNext: self.subject.onNext).dispose()
+		Observable.just(true).subscribe(onNext: self.testObserver.onNext).dispose()
+//		Observable.just(true).subscribe(onNext: self.publishSubject.onNext).dispose()
 	}
 
 }
